bfc177cd7f0c1fda5eefb631f81d9ef1bc716253
KD131 <71063280+KD131@users.noreply.github.com>
2023-03-10 23:03:06 +0100

    moves urls to urls module

---
+++ modules/urls.py
@@ -0,0 +1,29 @@
+proff_urls = [
+    'https://proff.dk/firma/dynatest-as/ballerup/producenter/0OCZCAI016D/',
+    'https://proff.dk/firma/eriksholm-research-centre/snekkersten/producenter/GL78U2I016D/',
+    'https://proff.dk/firma/formpipe-software-as/ballerup/internetdesign-og-programmering/0HDD4NI0C2C/',
+    'https://proff.dk/firma/novo-nordisk-as/bagsv%C3%A6rd/producenter/0EFWNQI016D/',
+    'https://proff.dk/firma/pfa-pension-forsikringsaktieselskab./k%C3%B8benhavn-%C3%B8/internetdesign-og-programmering/083DHKI0C2C/',
+    'https://proff.dk/firma/topdanmark-as/ballerup/forsikring/1AGO29I0ARC/',
+    'https://proff.dk/firma/kmd-as/ballerup/it-drift-og-support/0G0T8HI0ZDG/',
+    'https://proff.dk/firma/northtech-aps/k%C3%B8benhavn-k/internetdesign-og-programmering/060XOXI0C2C/',
+    'https://proff.dk/firma/alpha-solutions-as/k%C3%B8benhavn-k/internetdesign-og-programmering/0GDK54I0C2C/',
+    'https://proff.dk/firma/dafolo-as/frederikshavn/internetdesign-og-programmering/10W5QXI0C2C/',
+    'https://proff.dk/firma/nuuday-as/k%C3%B8benhavn-sv/telekommunikation/0NUYAJI01PE/',
+    'https://proff.dk/firma/netcompany-as/k%C3%B8benhavn-k/internetdesign-og-programmering/08TJ75I0C2C/'
+]
+
+website_urls = [
+    'https://dynatest.com/',
+    'https://www.eriksholm.com/',
+    'https://www.formpipe.com/',
+    'https://www.novonordisk.com/',
+    'https://english.pfa.dk/individual/',
+    'https://www.topdanmark.dk/',
+    'https://www.kmd.net/en',
+    'https://northtech.dk/',
+    'https://www.alpha-solutions.com/us/',
+    'https://dafolo.dk/',
+    'https://nuuday.com/',
+    'https://www.netcompany.com/en-GB/'
+]
