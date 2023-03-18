f2909d6a3a8db834eef9f8fd1aeaa98864d37a0f
KD131 <71063280+KD131@users.noreply.github.com>
2023-03-19 00:36:50 +0100

    adds more companies to the dataset

--- modules/urls.py
+++ modules/urls.py
@@ -10,7 +10,20 @@ proff_urls = [
     'https://proff.dk/firma/alpha-solutions-as/k%C3%B8benhavn-k/internetdesign-og-programmering/0GDK54I0C2C/',
     'https://proff.dk/firma/dafolo-as/frederikshavn/internetdesign-og-programmering/10W5QXI0C2C/',
     'https://proff.dk/firma/nuuday-as/k%C3%B8benhavn-sv/telekommunikation/0NUYAJI01PE/',
-    'https://proff.dk/firma/netcompany-as/k%C3%B8benhavn-k/internetdesign-og-programmering/08TJ75I0C2C/'
+    'https://proff.dk/firma/netcompany-as/k%C3%B8benhavn-k/internetdesign-og-programmering/08TJ75I0C2C/',
+    'https://proff.dk/firma/wash-world-aps/s%C3%B8borg/bilv%C3%A6rksteder/0KJ64VI0064/',
+    'https://proff.dk/firma/carve-consulting-ps/k%C3%B8benhavn-k/virksomhedsr%C3%A5dgivning/0KYPPII043Z/',
+    'https://proff.dk/firma/groupm-denmark-as/k%C3%B8benhavn-s/hovedkontortjenester/0I05XQI10NZ/',
+    'https://proff.dk/firma/br%C3%B8ndbyernes-i.f.-fodbold-as/br%C3%B8ndby/hoteller-og-andre-overnatningssteder/1DYZFMI00LQ/',
+    'https://proff.dk/firma/meew-as/k%C3%B8benhavn-k/holdingselskaper/0ONEYSI06Y4/',
+    'https://proff.dk/firma/funelo-aps/kongens-lyngby/internetdesign-og-programmering/0PPMS9I0C2C/',
+    'https://proff.dk/firma/precure-aps/allinge/producenter/0MQ796I016D/',
+    'https://proff.dk/firma/wilke-as/odense-c/markedsunders%C3%B8gelser/0CZWH9I010U/',
+    'https://proff.dk/firma/ooono-as/valby/elektronik-og-telekommunikationsudstyr-engros/0N4Z6UI10MZ/',
+    'https://proff.dk/firma/elbek-vejrup-as/aarhus-n/ejendomshandel-og-udleje/0BZ74UI10MV/',
+    'https://proff.dk/firma/ellab-as/hiller%C3%B8d/producenter/0CFW6TI016D/',
+    'https://proff.dk/firma/lejka-aps/k%C3%B8benhavn-n/ejendomsforvaltning-og-udvikling/0OMDJ7I00B0/',
+    'https://proff.dk/firma/firi-filial-af-firi-as/k%C3%B8benhavn-%C3%B8/anden-forretningsservice/0PGU1VI10LQ/'
 ]
 
 website_urls = [
@@ -25,5 +38,18 @@ website_urls = [
     'https://www.alpha-solutions.com/us/',
     'https://dafolo.dk/',
     'https://nuuday.com/',
-    'https://www.netcompany.com/en-GB/'
+    'https://www.netcompany.com/en-GB/',
+    'https://washworld.dk/',
+    'https://www.carve.dk/',
+    'https://www.groupm.com/',
+    'https://brondby.com/',
+    'https://www.meew.dk/',
+    'https://udvikling.funelo.io/',
+    'https://precure.dk/',
+    'https://wilke.dk/international',
+    'https://ooono.com/',
+    'https://elbek-vejrup.dk/',
+    'https://www.ellab.com/',
+    'https://www.lejka.dk/en',
+    'https://firi.com/'
 ]
