23deed557f07af27bb1bc07ba65f262dfa4428cb
KD131 <71063280+KD131@users.noreply.github.com>
2023-04-21 22:11:51 +0200

    refactors json_or_get to fetch and makes path optional

--- modules/utils.py
+++ modules/utils.py
@@ -1,17 +1,19 @@
 import json
 from os.path import exists
+from typing import Callable, Iterable, Union
 
-def json_or_get(path, keys, get_data):
+
+def json_or_fetch(keys: Union[str, Iterable[str]], fetcher: Callable, path: str=None) -> dict:
     """
     Loads JSON data from path, then iterates through the keys collection to see if any data is not present.
     
-    If that is the case, then it will use the get_data callback on the key element to get the data and add it.
+    If that is the case, then it will use the fetcher callback on the key element to get the data and add it.
 
     If data was added, it saves the data to the path as a new JSON file.
 
     :param path: Path to the JSON file.
     :param keys: Iterable of keys with which to store and get data.
-    :param get_data: Callback function for retrieving data. Should take a key as argument.
+    :param fetcher: Callback function for retrieving data. Should take a key as argument.
     
     :returns: Data in a dict.
     """
@@ -22,24 +24,24 @@ def json_or_get(path, keys, get_data):
     # Alternatively, you could save the returned dict to file, essentially deleting any data that was not retrieved.
     
     if isinstance(keys, str):
-        keys = [keys]
+        keys = keys,
     
     # init empty dict
     data = {}
     changed = False
     # loads from json file if exists
-    if exists(path):
+    if path and exists(path):
         with open(path, encoding='utf-8') as f:
             data = json.load(f)
 
     # if data from collection is not in the existing json file, we get data from callback
     for key in keys:
         if key not in data.keys():
-            data[key] = get_data(key)
+            data[key] = fetcher(key)
             changed = True
 
     # overwrite existing json file with fresh data if something new was added
-    if changed:
+    if path and changed:
         with open(path, 'w', encoding='utf-8') as f:
             json.dump(data, f)
     
